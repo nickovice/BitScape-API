d6ff8749f4313be636340f7adecbedf028d652d3
nickovice <nicolasviceconti@gmail.com>
2024-09-20 01:14:54 -0300

    Add shared module

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { CategoriesService } from '../categories/categories.service';
+import { ProductsService } from '../products/products.service';
+
+@Module({
+  providers: [CategoriesService, ProductsService],
+  exports: [CategoriesService, ProductsService]
+})
+export class SharedModule {}
